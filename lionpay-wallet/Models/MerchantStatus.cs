6bf4788d820ac8195fe0085d5f7fb2fdfb0da358
pid011 <pid011@outlook.com>
2025-12-17 18:38:01 +0900

    dotnet format

--- lionpay-wallet/Models/MerchantStatus.cs
+++ lionpay-wallet/Models/MerchantStatus.cs
@@ -7,10 +7,10 @@ public enum MerchantStatus
 {
     /// <summary>Inactive merchant (0)</summary>
     Inactive = 0,
-    
+
     /// <summary>Active merchant (1)</summary>
     Active = 1,
-    
+
     /// <summary>Suspended merchant (2)</summary>
     Suspended = 2
 }
